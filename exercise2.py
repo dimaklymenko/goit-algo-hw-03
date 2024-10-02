96a5de1c95bcc7347c7d710dc3494e33fe70d0a0
dimaklymenko <dimaliar228@gmail.com>
2024-10-02 17:41:39 +0300

    #3

--- exercise2.py
+++ exercise2.py
@@ -4,19 +4,22 @@ def get_numbers_ticket(min, max, quantity):
     try:
         if min <= 0 or max > 1000:
             empty_list = []
-            return print(f"min має бути >=1 та max має бути <=1000, {empty_list}")      # return empty_list щоб повернуло порожній список
+            print("min має бути >=1 та max має бути <=1000")
+            return empty_list
         elif min > max:
             empty_list = []
-            return print(f"min має бути менше за max {empty_list}")         # return empty_list щоб повернуло порожній список
-        elif len(range(min, max)) < quantity-1:
+            print("min має бути менше за max")
+            return empty_list
+        elif max-min < quantity:
             empty_list = []
-            return print(f"quantity-1 має бути <= len(range(min, max)), {empty_list}")          # return empty_list щоб повернуло порожній список
+            print("quantity має бути <= max-min")         
+            return empty_list
         else:
             lottery_numbers_set = set()
             while len(lottery_numbers_set) != quantity:
                 lottery_numbers_set.add(random.randint(min, max))
-            return sorted(list(lottery_numbers_set))    # return sorted(random.sample(range(min, max), quantity))       
+            return sorted((lottery_numbers_set))    # return sorted(random.sample(range(min, max), quantity))       
     except Exception:
         pass
-lottery_numbers = get_numbers_ticket(10, 15, 9)
+lottery_numbers = get_numbers_ticket(900, 1020, 30)
 print("Ваші лотерейні числа:", lottery_numbers)
